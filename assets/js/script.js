a6ed1f4b19f1d226ed999d7023fbea05247e6652
G. Godwin <g.godwin@Gs-MacBook-Pro.local>
2022-11-06 16:17:37 -0500

    questions index, question.answer array reformatted

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,12 @@ var timerElement = document.querySelector(".timer-count");
 var timer;
 var timerCount;
 var score;
-var results = document.querySelector(".btn");
+var results = document.querySelectorAll(".btn");
+console.log(results);
+
 //Leaderboard update
 var initialsEl = document.querySelector(".initials");
-var scoreEl = document.querySelector("score");
+var scoreEl = document.querySelector(".score");
 
 // Questions that the user will be answering
 var questionsEl = document.querySelector(".mcq-questions");
@@ -15,41 +17,42 @@ var answerButtonEl2 = document.querySelector("#answer2");
 var answerButtonEl3 = document.querySelector("#answer3");
 var answerButtonEl4 = document.querySelector("#answer4");
 var insertQuestion = []; //where the 'new' question needs to go
+var questionsIndex = 0
 var questions = [
     {   question: "What is a DOM?",
          answer1: "Short form of Domino",
          answer2: "Acronym for Document Object Model",
          answer3: "Short-hand for the word Domestic",
          answer4: "Acronym for Do Only Media Queries",
-         answer:2,
+         answer: "Acronym for Document Object Model",
     },
     {   question: "How can you get text to show onto HTML using JavaScript?",
          answer1: "getElementById",
          answer2: "querySelector",
          answer3: "appendChild",
          answer4: "textContent",
-         answer:4,
+         answer: "textContent",
     },
     {   question: "True or False is considered what type of variable?",
          answer1: "Undefined",
          answer2: "String",
          answer3: "Number",
          answer4: "Boolean",
-         answer:4,
+         answer: "Boolean",
     },
     {   question: "The following operator compares both the value and type:",
          answer1: "===",
          answer2: "&&",
          answer3: "||",
          answer4: "!=",
-         answer:1,
+         answer: "===",
     },
     {   question: "Styling primarily belongs to which language",
          answer1: "JavaScript",
          answer2: "HTML",
          answer3: "CSS",
          answer4: "API's",
-         answer:3,
+         answer: "CSS",
     }
 ];
 
@@ -86,32 +89,30 @@ function startTimer() {
     }, 1000);
   }
 
+// Select Answers to determine whether you lose time or move to the next question
+function selectAnswer(event){
+    var buttonEl = event.target;
+    console.log(buttonEl.innerText);
+
+//     console.log(questions[0].answer)
+//     if(buttonEl.innerText=== questions[0].answer){
+//         console.log("Correct!")
+//         questionsIndex++;
+//         showQuestion();
+//     }
+//      else {
+//         timerCount-15;
+//     }
+// };
+}
   //Display Questions and MCQ Answers
 function showQuestion() {
-    insertQuestion = questions[Math.floor(Math.random() * questions.length)];
-    questionsEl.textContent = insertQuestion.question;
-    answerButtonEl1.textContent = insertQuestion.answer1;
-    answerButtonEl2.textContent = insertQuestion.answer2;
-    answerButtonEl3.textContent = insertQuestion.answer3;
-    answerButtonEl4.textContent = insertQuestion.answer4;
-
-//Tests each click to the answer
-answerButtonEl1.onclick = selectAnswer();
-answerButtonEl2.onclick = selectAnswer();
-answerButtonEl3.onclick = selectAnswer();
-answerButtonEl4.onclick = selectAnswer();
-
-// Select Answers to determine whether you lose time or move to the next question
-function selectAnswer(event) {
-    var buttonEl = event.target;
-    event.target = addEventListener('click')
-    if (buttonEl.matches('.btn')){
-        console.log("hi")
-    } else {
-        console.log("try again")
-};
-
-}
+    // insertQuestion = questions[Math.floor(Math.random() * questions.length)];
+    questionsEl.textContent = questions[questionsIndex].question;
+    answerButtonEl1.textContent = questions[questionsIndex].answer1;
+    answerButtonEl2.textContent = questions[questionsIndex].answer2;
+    answerButtonEl3.textContent = questions[questionsIndex].answer3;
+    answerButtonEl4.textContent = questions[questionsIndex].answer4;
 }
 
 // If the time runs out or quiz is completed
@@ -132,3 +133,9 @@ function completeQuiz() {
     initialsEl.append(Name);
     scoreEl.append(score);
     }
+
+    results.addEventListener('click', selectAnswer())
+   for(var i = 0; i < results.length; i++) {
+        results[i].addEventListener('click', selectAnswer());
+    }
+    
